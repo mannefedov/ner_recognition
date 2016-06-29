7ffb7dbf4455527feb78f51efeda32296468c93b
Popov Mikhail <popovm105@yandex.ru>
2016-06-29 16:08:56 +0300

    add console interface

--- Text_Processing.py
+++ Text_Processing.py
@@ -5,42 +5,55 @@ import Extracting_Rules as Rules
 import Useful_functions as Use
 import Json_Handler as Jh
 from os.path import basename
+import sys
+import argparse
 
-# Путь, где лежит коллекция текстов для соревнований
-input_path = './testset/'
 
-# Папка с результатами
-result_path = './result/'
-try:
-    os.mkdir(result_path)
-except FileExistsError:
-    pass
+def createParser ():
+    parser = argparse.ArgumentParser()
+    # Путь, где лежит коллекция текстов для соревнований
+    parser.add_argument ('-i', '--input', default='./testset/')
+    # Папка с результатами
+    parser.add_argument ('-o', '--output', default='./result/')
+    return parser
 
-files = [input_path+file for file in  os.listdir(input_path)]
 
-for filename in sorted(files):
-    try:
-        if filename.endswith(".txt"):
-            f = codecs.open(filename, 'r')
-            #filename = filename.split('.')[0]
-            print("Обрабатывается файл", basename(filename))
-            text = Bf.pipeline(filename)
-            text = Rules.pipeline(text)
 
-            result = Jh.formatting(text)
-            for line in result:
-                print(line)
+if __name__ == '__main__':
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+    input_path = namespace.input
+    result_path = namespace.output
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
+    if os.path.exists(input_path):
+    
+        files = [input_path+file for file in  os.listdir(input_path)]
 
-            Use.write_as_object(result, basename(filename), path=result_path)
-    except FileNotFoundError as err:
-        print("Skipping 1 file", filename)
-        pass
-    except IsADirectoryError as err_2:
-        pass
-    except Exception as err_3:
-        print(err_3)
-        pass
+        for filename in sorted(files):
+            try:
+                if filename.endswith(".txt"):
+                    f = codecs.open(filename, 'r')
+                    #filename = filename.split('.')[0]
+                    print("Обрабатывается файл", basename(filename))
+                    text = Bf.pipeline(filename)
+                    text = Rules.pipeline(text)
 
+                    result = Jh.formatting(text)
+                    for line in result:
+                        print(line)
+
+                    Use.write_as_object(result, basename(filename), path=result_path)
+            except FileNotFoundError as err:
+                print("Skipping 1 file", filename)
+                pass
+            except IsADirectoryError as err_2:
+                pass
+            except Exception as err_3:
+                print(err_3)
+                pass
+    else:
+        print('Папки "{}" не существует'.format(input_path))
 
 # for filename in sorted(os.listdir(os.getcwd())):
 #     if not filename.split('.')[0].endswith('features'):
